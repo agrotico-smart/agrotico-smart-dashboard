8c58b1cf5d32c718be34257dadd41adba721d157
Esteban Canales <estebancomoprogramador@gmail.com>
2025-10-09 19:21:46 -0600

    Fix Vercel deployment configuration 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { getRobotsData } from "@/actions/dashboard";
-import Dashboard from "@/components/Dashboard";
+import DashboardServer from "@/components/DashboardServer";
 
 export default async function HomePage() {
   const { robots, lastUpdate } = await getRobotsData();
 
-  return <Dashboard initialRobots={robots} initialLastUpdate={lastUpdate} />;
+  return (
+    <DashboardServer initialRobots={robots} initialLastUpdate={lastUpdate} />
+  );
 }
-
-
